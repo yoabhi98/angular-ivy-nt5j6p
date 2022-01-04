80ec876e460fb6a77ad25b83790b15c3b707b1cf
yoabhi98 <yoabhi98@gmail.com>
2022-01-04 16:01:56 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { EmployeesService } from './employees.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  title: String = 'Http Service Demo';
+  employees: any;
+
+  constructor(private _employeesService: EmployeesService) {}
+  ngOnInit() {
+    this._employeesService
+      .getEmployees()
+      .subscribe((employees) => (this.employees = employees));
+  }
 }
