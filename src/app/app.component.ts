64cd32c52c3109bd2f1c372adaac822281607291
yoabhi98 <yoabhi98@gmail.com>
2022-01-04 17:12:01 +0530

    4 files modified, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,13 @@ import { EmployeesService } from './employees.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title: String = 'Http Service Demo';
   employees: any;
 
   constructor(private _employeesService: EmployeesService) {}
+
   ngOnInit() {
     this._employeesService
       .getEmployees()
--- src/app/employees.service.ts
+++ src/app/employees.service.ts
@@ -1,9 +1,11 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class EmployeesService {
-  private path = '../assets/employees.json';
+  private path = '../assets/emp.json';
 
   constructor(private _http: HttpClient) {}
 
